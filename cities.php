0968a915f3514a3e8f972cdc55210a9502d4fc42
jesus <jesus.fs.franco@gmail.com>
2013-03-14 18:07:15 +0100

    version principio

---
+++ cities.php
@@ -0,0 +1,25 @@
+<html>
+<title>Cities of the World
+</title>
+<body>
+
+
+<?php
+$con=mysqli_connect("localhost","root","123","world");
+$result = mysqli_query($con,"" );
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+
+?>
+
+
+
+</body>
+
+
+
+</html>
