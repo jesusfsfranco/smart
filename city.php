ee143f0b8fdf7fcc3aa09f06dfe7e154aa898746
jesus <jesus.fs.franco@gmail.com>
2013-03-14 19:22:22 +0100

    version nueva acabada

--- city.php
+++ city.php
@@ -9,11 +9,12 @@
 $ciudad = $_GET["id"];
 
 $con=mysqli_connect("localhost","root","123","world");
-$result = mysqli_query($con,"select " );
 if (mysqli_connect_errno($con))
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+  
+  
 $result = mysqli_query($con,"SELECT ID, CountryCode, Name, District, Population FROM city WHERE ID=$ciudad"); 
 $fila = $result->fetch_assoc();
 $form_CountryCode = $fila['CountryCode'];
@@ -21,27 +22,32 @@ $form_Name = $fila['Name'];
 $form_District = $fila['District'];
 $form_Pop = $fila['Population'];  
 
+$result2 = mysqli_query($con,"SELECT Name FROM country WHERE Code='$form_CountryCode'"); 
+$fila2 = $result2->fetch_assoc();
+$form_Country = $fila2['Name'];
+
 //PUNTO  5 tengo que comprobar que reusltado es correcto
 
 
 
+displayWiki($form_Name, $form_CountryCode,$form_Pop,$form_District,$form_Country);
 
 
+function displayWiki($form_Name, $form_CountryCode,$form_Pop,$form_District,$form_Country){
 
-function displayWiki($form_Name){
-
-echo "<h1>$form_Name</h1>";
-echo "<p>country.php?Code=$form_CountryCode</p>";
-
-
+echo "<h1>Nombre de la ciudad: $form_Name</h1>"; echo "<br />";
+echo "<p><a href=country.php?Code=$form_CountryCode>LINK</a></p>";echo "<br />";
+echo "<p>Population: $form_Pop</p>";echo "<br />";
+echo "<p>District: $form_District </p>";echo "<br />";
+echo "<p><a href=http://en.wikipedia.org/wiki/$form_Country>WIKIPEDIA</a></p>";echo "<br />";
 
 }
 
 
 
+/*
 
 
-echo "<h1></h1>";
 echo "<p>Country Code: Country Code Link</p>(this link will open up the country
 web page – country.php?Code=CountryCode - to display the details of the
 country).";
@@ -57,13 +63,13 @@ echo "9. Close out and save the city.php file.";
 echo "10. View the script using the following URL:";
 
 
-}
 
 
 
 
 
 
+*/
 
 
 
