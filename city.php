109e75ab836f62fb6a1a6d5484472c15e158b31c
jesus <jesus.fs.franco@gmail.com>
2013-03-18 19:33:15 +0100

    Tengo que revisar el error de la ID que según pase la ID mayúscula o minúscula pasa según que cosas

--- city.php
+++ city.php
@@ -6,15 +6,15 @@
 
 <?php
 
-$ciudad = $_GET["id"];
+if(!empty($_GET["ID"])){
+
+$ciudad = $_GET["ID"];
 
 $con=mysqli_connect("localhost","root","123","world");
 if (mysqli_connect_errno($con))
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  
-  
+  }  
 $result = mysqli_query($con,"SELECT ID, CountryCode, Name, District, Population FROM city WHERE ID=$ciudad"); 
 $fila = $result->fetch_assoc();
 $form_CountryCode = $fila['CountryCode'];
@@ -26,13 +26,6 @@ $result2 = mysqli_query($con,"SELECT Name FROM country WHERE Code='$form_Country
 $fila2 = $result2->fetch_assoc();
 $form_Country = $fila2['Name'];
 
-//PUNTO  5 tengo que comprobar que reusltado es correcto
-
-
-
-displayWiki($form_Name, $form_CountryCode,$form_Pop,$form_District,$form_Country);
-
-
 function displayWiki($form_Name, $form_CountryCode,$form_Pop,$form_District,$form_Country){
 
 echo "<h1>Nombre de la ciudad: $form_Name</h1>"; echo "<br />";
@@ -43,37 +36,9 @@ echo "<p><a href=http://en.wikipedia.org/wiki/$form_Country>WIKIPEDIA</a></p>";e
 
 }
 
+displayWiki($form_Name, $form_CountryCode,$form_Pop,$form_District,$form_Country);
 
-
-/*
-
-
-echo "<p>Country Code: Country Code Link</p>(this link will open up the country
-web page – country.php?Code=CountryCode - to display the details of the
-country).";
-
-
-echo "<p>Population: Size of the city population</p>";
-echo "<p>District: Name of the District the city belongs to</p>";
-echo "<p>Wikipedia Link</p>(this link will open up a Wikipedia web page -
-http://en.wikipedia.org/wiki/Name of City)";
-echo "7. Add to the script code that will empty (clean out) the result set.";
-echo "8. Add to the script code that will disconnect from the MySQL server.";
-echo "9. Close out and save the city.php file.";
-echo "10. View the script using the following URL:";
-
-
-
-
-
-
-
-
-*/
-
-
-
-
+}else{ echo "<h1>ERROR EN EL PASO DE DATOS</h1><br /><a href='city.php?ID=1'>VER LA PRIMERA CIUDAD</a>";}
 
 
 
@@ -86,3 +51,8 @@ echo "10. View the script using the following URL:";
 
 
 </html>
+
+
+
+
+
